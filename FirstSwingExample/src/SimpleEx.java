7cf0a92a8edd015c0d17ad82c864ef7e6b9219e2
kswr <ksawery.oleszynski@gmail.com>
2018-05-20 09:41:37 +0200

    code example from zetcode

---
+++ FirstSwingExample/src/SimpleEx.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SimpleEx extends JFrame {
+
+    public SimpleEx() {
+        initUI();
+    }
+
+    private void initUI() {
+        setTitle("Simple example");
+        // use pack() to make window fit it's content
+        setSize(600, 400);
+        // displays on the center
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            SimpleEx ex = new SimpleEx();
+            ex.setVisible(true);
+        });
+
+    }
+}
---
+++ HelloWorldSwing/src/com/company/NewHello.java
@@ -0,0 +1,7 @@
+package com.company;
+
+import javax.swing.*;
+
+public class NewHello {
+    private JTextArea textArea1;
+}
